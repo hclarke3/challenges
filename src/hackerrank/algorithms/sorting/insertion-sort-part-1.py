ef50b43738c6f6644ca2780dda3deca4212fd361
Hunter Clarke <hunter@hunterc.com>
2014-01-03 21:10:05 -0500

    completed hackerrank challenge 'insertion sort - part 1'

---
+++ src/hackerrank/algorithms/sorting/insertion-sort-part-1.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+def printar(ar, ret):
+    ret = ret + str(ar[0])
+    for i in range(1, len(ar)):
+        ret = ret + " " + str(ar[i])
+        
+    return ret+"\n"
+
+# Head ends here
+def insertionSort(ar):    
+    ret = ""
+    tail = ar[len(ar)-1]
+    i = len(ar) - 2
+
+    while (i >= 0):
+        if (ar[i] < tail):
+            ar[i+1] = tail
+            break
+        else:
+            ar[i+1] = ar[i]
+            i = i - 1
+        
+        ret = printar(ar, ret)
+    
+    ret = printar(ar, ret)     
+    print ret
+
+# Tail starts here
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+insertionSort(ar)
